19560d8b2bc0edbc24e4d558383f48bf155b46be
Izhaan Salam <izhaan.salam@student.manchester.ac.uk>
2025-06-12 15:47:44 +0100

    Add pages deployment

--- zeema/next.config.ts
+++ zeema/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Remove this if your repo name is not zeema_page
+  basePath: process.env.NODE_ENV === 'production' ? '/zeema_page' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/zeema_page/' : '',
 };
 
 export default nextConfig;
